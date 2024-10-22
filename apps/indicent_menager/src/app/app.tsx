d54ade189331e9dc02ffedb942a6e42a0b101042
maciek30-byte <maciek29.opozda@gmail.com>
2024-10-22 13:57:07 +0200

    PROJECT-1 Config Modular monolit setup base structure

---
+++ apps/indicent_menager/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="indicent_menager" />
+    </div>
+  );
+}
+
+export default App;
---
+++ libs_fe/ui-components/src/lib/ui-components.tsx
@@ -0,0 +1,15 @@
+import styled from 'tailwind';
+
+const StyledUiComponents = styled.div`
+  color: pink;
+`;
+
+export function UiComponents() {
+  return (
+    <StyledUiComponents>
+      <h1>Welcome to UiComponents!</h1>
+    </StyledUiComponents>
+  );
+}
+
+export default UiComponents;
